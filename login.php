d06bc3d51b57be1ce23497968738a906a590b8cc
Prabhakar S <prabhakar.gts@gmail.com>
2019-12-24 15:59:20 +0530

    Uploaded By Doddanna
    Latest code developed by Joshua

---
+++ login.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE html>
+<html  >
+<head>
+ 
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/Logo.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Swami Vivekananda Jnana Kendra</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link href="assets/font/css/all.css" rel="stylesheet">
+  <style>
+  .selectpicker{
+  border:none;
+	padding: 10px 30px 0 0;
+	font-size: 35px;
+	margin: 0 10px;
+	color: #1d2050;
+	 font-family: arimo-bold;
+	 -webkit-appearance: none;  /*Removes default chrome and safari style*/
+			-moz-appearance: none;
+	background: transparent url(images/down-arrow.png) no-repeat 100% 70%;
+ }
+ #map {
+  height: 400px;  /* The height is 400 pixels */
+  width: 100%;  /* The width is the width of the web page */
+ }
+ .form-check-input::before {
+    background-color: lightgreen;
+}
+#select-search{
+  background:#fff;
+}
+#btn-search{
+  background:#ffb833;
+}
+.form-controls{
+display:block;
+width:100%;
+height:39px;
+padding:6px 12px;
+font-size:14px;
+line-height:1.42857143;
+color:#555;
+background-color:#fff;
+background-image:none;
+border:1px solid #ccc;
+border-radius:4px;
+-webkit-box-shadow:inset 0 1px 1px rgba(0,0,0,.075);box-shadow:inset 0 1px 1px rgba(0,0,0,.075);-webkit-transition:border-color ease-in-out .15s;
+box-shadow ease-in-out .15s;-o-transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s;-webkit-transition:border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;transition:border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s;transition:border-color ease-in-out .15s,box-shadow ease-in-out .15s,-webkit-box-shadow ease-in-out .15s.	
+}  
+.form-controls:focus{border-color:#66afe9;outline:0;-webkit-box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6);box-shadow:inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)}
+.form-controls::-moz-placeholder{color:#999;opacity:1}
+.form-controls:-ms-input-placeholder{color:#999}
+.form-controls::-webkit-input-placeholder{color:#999}
+#btn{
+width:100%;
+height:39px;
+color:#fff;
+background-color:#4285F4;
+border:1px solid #4285F4;
+border-radius:5px;
+margin-bottom:25px;
+cursor:pointer;
+}
+#btn-google{
+width:49%;
+height:39px;
+float:left;
+color:#fff;
+background-color:#ea4335;
+border:1px solid #ea4335;
+border-radius:5px;
+margin-bottom:25px;
+cursor:pointer;
+}
+#btn-facebook{
+width:50%;
+height:39px;
+color:#fff;
+margin-left:1%;
+background-color:#4285F4;
+border:1px solid #4285F4;
+border-radius:5px;
+margin-bottom:25px;
+cursor:pointer;
+}
+</style>
+  
+  
+</head>
+<body style="background:#f2f2f2;">
+ <!-- Login section -->
+ <section id="userlogin">
+ <div class="container-fluid" id="login-section">
+ <div class="row">
+ <div class="col-md-5" style="height:420px;background:#fff;margin:100px auto;border:1px solid #f4f4f4;border-radius:4px;">
+   <div class="center-box-login">
+   <div class="login-header" style="width:100%;height:60px;margin-top:10px;">
+   <a href="" class="display-2" style="font-size:28px;float:left;margin-left:100px;border-bottom:1.5px solid #4285F4;color:#000;">Login</a>
+   <a href="" class="display-2" style="font-size:28px;float:right;margin-right:100px;color:#000;">Register</a>
+   </div>
+   <div class='login-box-input group-form' style="width:70%;margin:2px auto;">
+    <form>
+    <div class="form-group">
+    <label for="exampleInputEmail1" class="display-4">Email or Phone</label>
+    <input type="email" class="form-controls" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="display-4">Password</label>
+    <input type="password" class="form-controls" id="exampleInputPassword1">
+  </div>
+  <div class="forgetpassword" style="float:right;margin-bottom:10px;">
+    <a href="" class="display-4">Forget  username / Password</a>
+    </div>
+    <div style="clear:both;">
+    </div>
+    <div class="submitbtn">
+     <input id="btn" value="Sign In" type="submit" >
+    </div>
+    <div class="loginGmail">
+    <button id="btn-google"><i class="fab fa-google"></i>&nbsp;&nbsp;Sign  with Gmail</button>
+    <button id="btn-facebook"><i class="fab fa-facebook-f"></i>&nbsp;&nbsp;Sign with Facebook</button>
+    <div class="clear:both">
+
+    </div>
+    </form>
+   </div>
+   </div>
+ </div>
+ </div>
+ </div>
+ </section>
+
+
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/mbr-flip-card/mbr-flip-card.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/sociallikes/social-likes.js"></script>
+  <script src="assets/mbr-tabs/mbr-tabs.js"></script>
+  <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+   <script defer src="assets/font/js/all.js"></script>
+  
+  
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,260 @@
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v4.9.7, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/Logo.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Swami Vivekananda Jnana Kendra</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+</head>
+<body>
+  <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-1k">
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+      
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.php">
+                         <img src="assets/images/Logo.png" alt="" style="height: 4.8rem;">
+                    </a>
+                </span>
+              
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="index.php">
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="about.php">
+                        About Us
+                    </a>
+                </li>
+				 <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="services.php">
+                        Educational Services
+                    </a>
+                </li>
+				 <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="contact.php">
+                        Contact Us
+                    </a>
+                </li>
+            </ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/g">Swami Vivekananda Jnana Kendra</a></section><section class="header1 cid-rm0rYIbKvu mbr-parallax-background" id="header1-1t">
+<div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(20, 157, 204);">
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">               
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-2">
+                   Swami Vivekananda Jnana Kendra
+                </h3>
+               
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="engine"><a href="https://mobirise.info/j"></a></section><section class="mbr-section form4 cid-rm0unb6mMM" id="form4-23">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+                <div>
+                    <div class="icon-block pb-3">
+                        <span class="icon-block__icon">
+                            <span class="mbri-letter mbr-iconfont"></span>
+                        </span>
+                        
+                    </div>                   
+                </div>
+                <div data-form-type="formoid"  style="display:float;" >
+                    <!---Formbuilder Form--->
+                    <form action="signup.php" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form">
+					<div class="section-title lines">
+                    <h3>
+                        <span class="text">Sign up using</span>
+                    </h3>
+                </div>
+				<center><?php// require './fb.php'; ?>
+                        <button type="button" style="background-color:Blue;padding: 10px;border-radius: 12px;" class="button button-primary button-oa2-facebook login-provider register-oa2-facebook" data-provider-url="/auth/login/facebook/?auth_entry=register&amp;next=%2Fdashboard">
+                            <div class="icon " aria-hidden="true">
+                                
+                                    <a href="<?php echo $login_url; ?>"><img class="icon-image" src="https://edxuploads.s3.amazonaws.com/flogo-HexRBG-Wht-58.svg" alt="Facebook icon" height="50px" width="50px">
+                                </a>
+                            </div>
+                        </button>
+						<span style="padding:50px;"></span>
+                        <button type="button" style="background-color:white;padding: 10px;border-radius: 12px;" class="button button-primary button-oa2-google-oauth2 login-provider register-oa2-google-oauth2" data-provider-url="/auth/login/google-oauth2/?auth_entry=register&amp;next=%2Fdashboard">
+                            <div class="icon " aria-hidden="true">
+                                
+                                    <img class="icon-image" src="https://edxuploads.s3.amazonaws.com/btn_google_light.svg" alt="Google icon" height="50px" width="50px">
+                                
+                            </div>
+            
+                        </button>
+						<span style="padding:50px;"></span>
+                        <button type="button" style="background-color:white;padding: 10px;border-radius: 12px;" class="button button-primary button-oa2-azuread-oauth2 login-provider register-oa2-azuread-oauth2" data-provider-url="/auth/login/azuread-oauth2/?auth_entry=register&amp;next=%2Fdashboard">
+                            <div class="icon " aria-hidden="true">
+                                
+                                    <img class="icon-image" src="https://dmtyylqvwgyxw.cloudfront.net/instances/222/uploads/images/custom_image/image/35615/square-linkedin-512.png?v=1553018802" alt="Microsoft icon" height="50px" width="50px" >
+                                
+                            </div>
+                        </button>
+						<span style="padding:50px;"></span>
+						</center>
+                </div>
+
+                
+            </div>
+			<div class="section-title lines">
+                <h3>
+                    <span class="text">or create a new one here</span>
+                </h3>
+            </div>
+					 <div class="dragArea row">
+					  <div data-for="email" class="col-md-12  form-group">
+                                <input type="text" name="email" placeholder="Email" data-form-field="Email" class="form-control input display-7" required="required" id="email-form4-23">
+                            </div>
+							 <div data-for="password" class="col-md-12  form-group">
+                                <input type="text" name="Password" placeholder="Password" data-form-field="Password" class="form-control input display-7" required="required" id="password-form4-23">
+                            </div>
+							 <div data-for="cpassword" class="col-md-12  form-group">
+                                <input type="text" name="CPassword" placeholder="Confirm Password" data-form-field="CPassword" class="form-control input display-7" required="required" id="cpassword-form4-23">
+                            </div>
+                            <div class="col-md-12  form-group" data-for="name">
+                                <input type="text" name="name" placeholder="Your Name" data-form-field="Name" required="required" class="form-control input display-7" id="name-form4-23">
+                            </div>
+							<div class="col-md-12  form-group" data-for="dob">
+                                <input type="text" placeholder="Date of Birth" onfocus="(this.type='date')" name="dob" placeholder="Date of Birth" data-form-field="DOB" required="required" class="form-control input display-7" id="dob-form4-23">
+                            </div>
+                            <div class="col-md-12  form-group" data-for="phone">
+                                <input type="text" name="phone" placeholder="Phone" maxlength="10" size="10" data-form-field="Phone" required="required" class="form-control input display-7" id="phone-form4-23">
+                            </div>
+							
+						<div class="col-md-12  form-group">
+						<div>
+						  <select class="form-control input display-7" data-form-field="gender" required="required"  id="gender-form4-23" style="border-radius:5px;padding:0px;margin:0px;">
+							  <option disabled selected>Gender</option>
+							  <option>Male</option>
+								<option>Female</option>
+								<option>Other</option>
+						  </select>
+						  </div>	  </div>
+                            	<div class="col-md-12  form-group">
+						  <select class="form-control input display-7" data-form-field="type" required="required"  id="type-form4-23" style="border-radius:5px;padding:0px;margin:0px;">
+							  <option disabled selected>Category</option>
+							  <option>Student</option>
+								<option>Faculty</option>
+						  </select>
+						  </div>	
+                            <div class="col-md-12 input-group-btn  mt-2 align-center">
+                                <button type="submit" class="btn btn-primary btn-form display-4">Sign up</button>
+                            </div>
+                        </div>
+                    </form><!---Formbuilder Form--->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cid-rm0rlkkphC" id="footer1-1r">
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                     <a href="#">
+                        <img src="assets/images/Logo.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">
+                    #184, Hennur Cross, Near Indian Academy College,Off Ring Road, Bengaluru-560043 Karnataka, India.
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: svj.kendra@gmail.com
+                    <br>Phone: 080-41722223
+                    <br>Mobile No: 9741264243, 9731663600
+                </p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Quick Links
+                </h5>
+                <p class="mbr-text">
+                    <a class="text-primary" href="about.html">About</a>
+                    <br><a class="text-primary" href="services.html">Services</a>
+                    <br><a class="text-primary" href="contact">Contact</a>
+                </p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-8 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2019 Swami Vivekananda Jnana Kendra - All Rights Reserved
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/mbr-flip-card/mbr-flip-card.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/sociallikes/social-likes.js"></script>
+  <script src="assets/mbr-tabs/mbr-tabs.js"></script>
+  <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+</body>
+</html>
