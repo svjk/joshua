eefea6f784623d4310b61c03c06baa56f8515720
doddanna d <chawannaikd@gmail.com>
2020-01-10 00:50:43 +0530

    Date 10-01-2020 worked on basic and medium search

--- dodda/index.php
+++ dodda/index.php
@@ -36,22 +36,20 @@
     <div></div>
 
 
-    <div class="site-section" style="background: rgb(172, 255, 82);">
+    <div class="site-section basic-search" style="">
       <div class="container">        
         <section class="search-section">
           <form action="#" method="post" class="basic-search-form">
             <div class="row">
               <div class="col-md-12 ">
-                <div class="row">
-                  <div class="search-title text-center">
-                    <h3>For all your coaching needs!</h3>
-                  </div>                  
+                <div class="search-title text-center">
+                  <h3>For all your coaching needs!</h3>
                 </div>
               </div>
               <div class="col-md-12">
                 <div class="row">
                   <div class="col-md-3  p-0">
-                    <select class="form-control " id="">
+                    <select class="form-control search-slt search-field-1" id="">
                       <option value="">Select</option>
                       <option>Student one</option>
                       <option>Faculty one</option>
@@ -59,17 +57,17 @@
                   </div>
 
                   <div class="col-md-3  p-0">
-                    <input type="text" class="form-control " placeholder="Enter Location">
+                    <input type="text" class="form-control search-slt" placeholder="Enter Location">
                   </div>
                   <div class="col-md-3  p-0">
-                    <select class="form-control " id="">
+                    <select class="form-control search-slt" id="">
                       <option>Select Subject</option>
                       <option>Subject 1</option>
                       <option>Subject 2</option>
                     </select>
                   </div>
                   <div class="col-md-3  p-0">
-                    <button type="button" class="btn btn-primary">Search</button>
+                    <button type="button" class="btn btn-primary wrn-btn">Search</button>
                   </div>
                 </div>
               </div>
@@ -80,81 +78,202 @@
     </div>
     <!-- end basic search -->
 
-    <!-- start adance search -->
-    <div class="site-section" style="background: rgb(172, 255, 82, .5);">
+    <!-- start medium search -->
+    <div class="site-section medium-search">
       <div class="container">        
-        <section>
-          <div class="row">
-            <div class='col-md-6 col-sm-12 col-xs-6'>
-              <div class="" style="">
-                <h1 class="display-2">Get the Best <br> Quality Teachers<br> Near You</h1>
-                <br>
-                <p>
-                  Our rigourous process and review mechanism separate best from the rest. You get only quality tutors at the convenience of your home.
-                </p>
+        <section class="">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-6" style="">
+                <div class="medium-search-title">
+                  <h3>
+                    Get the Best <br> Quality Teachers<br> Near You
+                  </h3>
+                  <p>
+                    Our rigourous process and review mechanism separate best from the rest. You get only quality tutors at the convenience of your home.
+                  </p>
+                </div>
               </div>
-            </div>
-            <!-- search panel -->
-            <div class='col-md-6 col-sm-12 col-xs-6' id="search-panel">
-              <div class='form-search' style="">
-                <div clas='search-detail' style="">
-                  <div class="form-check" style="">
-                    <input type="checkbox" class="form-check-input" id="home" checked><i class="fas fa-home" style="opacity:0.7"></i>
-                    <label class="form-check-label" for="home" style="">Home Tuition</label>
-                    <input type="checkbox" class="form-check-input" id="online"><i class="fas fa-laptop" style="opacity:0.7"></i>
-                    <label class="form-check-label" for="online" style="">Online</label>
-                    <input type="checkbox" class="form-check-input" id="center"><i class="fab fa-centercode" style="opacity:0.7"></i>
-                    <label class="form-check-label" for="center">Center</label>
-                  </div>
-                  <p id="text-info-search">High speed internet is required for online tuition.</p>
-                  <form class="form-group">
-                    <h4 class="display-4">Category</h4>
-                    <div class="form-group">
-                      <select class="form-controls">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </select>
+              <div class="col-md-6">
+                <div class="search-panel">
+                  <form action="" method="" class="">
+                    <div class="col-md-12">
+                      <div class="row">
+                        <div class="col-md-4">
+                          <div class="form-group form-check">                             
+                            <label class="form-check-label checkbox-labels" for="home">
+                              <input type="checkbox" class="form-check-input" id="home" checked> <i class="fas fa-home" style=""></i> Home Tuition
+                            </label>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group form-check">
+                            <label class="form-check-label checkbox-labels" for="online">
+                              <input type="checkbox" class="form-check-input" id="online"> <i class="fas fa-laptop" style=""></i> Online
+                            </label>
+                          </div>
+                        </div>
+                        <div class="col-md-4">
+                          <div class="form-group form-check">                            
+                            <label class="form-check-label checkbox-labels" for="center">
+                              <input type="checkbox" class="form-check-input" id="center"> <i class="fab fa-centercode" style=""></i> Center
+                            </label>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <p class="text-center note-for-online">
+                            <small>High speed internet is required for online tuition.</small>
+                          </p>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <label>Category</label>
+                              <select class="form-control">
+                                <option value="">Select Category</option>
+                                <option value="">Category List</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <label>Subject</label>
+                              <select class="form-control">
+                                <option value="">Select Subject</option>
+                                <option value="">Subject List</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <label>Location</label>
+                              <select class="form-control">
+                                <option value="">Select Location</option>
+                                <option value="">Location List</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="text-center">
+                              <button class="btn btn-primary">
+                                <i class="fas fa-search"></i> Get quality teachers
+                              </button>
+                            </div>
+                          </div>
+                        </div>                        
+                      </div>
                     </div>
-                    <h4 class="display-4">Subject:</h4>
-                    <div class="form-group">
-                      <select class="form-controls">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                      </select>
-                    </div>
-                    <h4 class="display-4">Location</h4>
-                    <div class="form-group">
-                      <select class="form-controls">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                      </select>
-                    </div>
-                    <button type="submit" id="btn"  style="width:100%;margin-left:-2px;"><i class="fas fa-search"></i>&nbsp;&nbsp;Get quality teachers</button>
                   </form>
                 </div>
               </div>
             </div>
-            <!-- end search panel -->
           </div>
         </section>
       </div>
     </div>
-    <!-- end advanced search -->
+    <!-- end medium search -->
+
+    <!-- searched results -->
+    <div class="site-section">
+      <div class="container">
+        <section>
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="tutorial-locations" style="height: 550px;width: 100%;">
+                  <div id="map" style="width: 100%;height: 100%;"></div>
+                </div>
+              </div>
+              <div class="col-md-8"> 
+                <div class="tutorial-list" style="border: 1px solid #000;height: 550px;">
+                  <section class="" >
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="result-title">
+                          <h5>Home Tution</h5>
+                        </div>
+                      </div>
+                      <div class="col-md-12">
+                        <div class="">
+                          <h5>List Goes here</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <!-- end searched results -->
+
+    <!-- achivements section -->
+    <div class="site-section" style="background-image: url(images/bg_4.jpg);" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <section class="ftco-section ftco-counter img" id="section-counter">
+          <div class="container">
+            <div class="row justify-content-center mb-5 pb-2">
+              <div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
+                <h2 class="mb-4"><span> Years of</span> Experience</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+              </div>
+            </div>  
+            <div class="row d-md-flex align-items-center justify-content-center">
+              <div class="col-lg-10">
+                <div class="row d-md-flex align-items-center">
+                  <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                    <div class="block-18">
+                      <div class="icon"><span class="flaticon-doctor"></span></div>
+                      <div class="text">
+                        <strong class="number" data-number="18">0</strong>
+                        <span>Certified Teachers</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                    <div class="block-18">
+                      <div class="icon"><span class="flaticon-doctor"></span></div>
+                      <div class="text">
+                        <strong class="number" data-number="351">0</strong>
+                        <span>Successful Kids</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                    <div class="block-18">
+                      <div class="icon"><span class="flaticon-doctor"></span></div>
+                      <div class="text">
+                        <strong class="number" data-number="564">0</strong>
+                        <span>Happy Parents</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+                    <div class="block-18">
+                      <div class="icon"><span class="flaticon-doctor"></span></div>
+                      <div class="text">
+                        <strong class="number" data-number="300">0</strong>
+                        <span>Awards Won</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+    <!-- end achivements section -->
 
     <!-- // 05 - Block -->
   <div class="site-section">
@@ -199,7 +318,20 @@
   </div>
   <!-- .site-wrap -->
   <?php include 'script_links.php'; ?>
-
+  <script src="js/jquery.animateNumber.min.js"></script>
 </body>
 
 </html>
+
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBM_uULzojkkhIew706uqwf8KG75oEVuDQ  &callback=initMap">
+</script><script>function initMap() {
+  var bangalore = {lat: 12.9833, lng: 77.5833};
+  var mark2 = {lat: 12.9933, lng: 77.6033};
+
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 13, center: bangalore});
+
+  var marker = new google.maps.Marker({position: bangalore, label: 'B', map: map});
+  var marker2 = new google.maps.Marker({position: mark2, label: 'A',map: map});
+}</script>
