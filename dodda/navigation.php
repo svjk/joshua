a7655d41c97bd7930c4d9d9bf0e6c4a9680ffb17
Prabhakar S <prabhakar.gts@gmail.com>
2020-01-11 21:39:22 +0530

    Update navigation.php

--- dodda/navigation.php
+++ dodda/navigation.php
@@ -16,8 +16,8 @@
 				<a href="#" class="small mr-3"><span class="icon-envelope-o mr-2"></span> svj.kendra@gmail.com</a> 
 			</div>
 			<div class="col-lg-3 text-right">
-				<a href="login.php" class="small mr-3"><span class="icon-unlock-alt"></span> Log In</a>
-				<a href="register.php" class="small btn btn-primary px-4 py-2 rounded-0"><span class="icon-users"></span> Register</a>
+				<!-- <a href="login.php" class="small mr-3"><span class="icon-unlock-alt"></span> Log In</a> -->
+				<a href="#" class="small btn btn-primary px-4 py-2 rounded-0"><span class="icon-users"></span> Register</a>
 			</div>
 		</div>
 	</div>
