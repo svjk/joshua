3a426911b600f237b7a8970a78fe7c0706a9c0f3
doddanna d <chawannaikd@gmail.com>
2020-01-24 18:05:45 +0530

    Date : 24-01-2020
    Date : 24-01-2020
    SMS OTP and MESSAGE SENDIING

--- dodda/send/sendsms.php
+++ dodda/send/sendsms.php
@@ -1,15 +1,20 @@
 <?php
-$token = 'Your Token Here';
-$mobile = mysql_real_escape_string($_POST['phone']);
-$msg = mysql_real_escape_string($_POST['message']);
-$site = 'Your Site Address Here';
-$url = "http://api.fast2sms.com/sms.php?token=".$token."&mob=".$mobile."&mess=".$msg."&sender=".$site."&route=0";
-$homepage = file_get_contents($url);
-if($homepage)
-{
-  echo "Message Send Compleated...";
-}
-else{
-  echo "Something Went Wrong...";
-}
-?>
+
+$YourAPIKey='33119aa2-46a6-11e9-8806-0200cd936042';
+$From='TFCTOR';
+$To='+919731263208';
+
+$Msg='Dear Sir/Medam this is doddanna from svjk';
+
+
+### DO NOT Change anything below this line
+$agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+$url = "https://2factor.in/API/V1/$YourAPIKey/ADDON_SERVICES/SEND/PSMS"; 
+$ch = curl_init(); 
+curl_setopt($ch,CURLOPT_URL,$url); 
+curl_setopt($ch,CURLOPT_POST,true); 
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
+curl_setopt($ch,CURLOPT_POSTFIELDS,"From=$From&To=$To&Msg=$Msg"); 
+curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+echo curl_exec($ch); 
+curl_close($ch);
